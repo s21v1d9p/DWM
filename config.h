799fae05e870b0f5b5571f00f05aed412ca1d34c
s21v1d9p <svdp028@gmail.com>
2021-08-07 12:41:22 +0530

    fibonacci layout added

--- config.h
+++ config.h
@@ -76,6 +76,7 @@ static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
+#include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
@@ -83,6 +84,9 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 	{ "|M|",      centeredmaster },
 	{ ">M>",      centeredfloatingmaster },
+ 	{ "[@]",      spiral },
+ 	{ "[\\]",      dwindle },
+
 };
 
 /* key definitions */
@@ -130,6 +134,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_y,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY|ShiftMask,             XK_y,      setlayout,      {.v = &layouts[4]} },
+	{ MODKEY,                       XK_i,      setlayout,      {.v = &layouts[5]} },
+	{ MODKEY|ShiftMask,             XK_i,      setlayout,      {.v = &layouts[6]} },
 	{ MODKEY,                       XK_p,  	   setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_p,      togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
