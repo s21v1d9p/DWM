c84516c53f933ba4533e80f24c248d30d038f35a
s21v1d9p <svdp028@gmail.com>
2021-08-21 08:22:20 +0530

    pywal portion removed and switch to static color

--- config.h
+++ config.h
@@ -21,19 +21,20 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {	"JetBrains Mono SemiBold:size=11", "Noto Color Emoji:size=9:antialias=true:autohint=true" };
-#include "/home/svdp/.cache/wal/colors-wal-dwm.h"
-//static const char col_gray1[]       = "#222222";
-//static const char col_gray2[]       = "#444444";
-//static const char col_gray3[]       = "#bbbbbb";
-//static const char col_gray4[]       = "#eeeeee";
-//static const char col_cyan[]        = "#005577";
-//static const char col_urgborder[]   = "#ff0000";
-//static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-//	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-//	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-//	[SchemeUrg]  = { col_gray4, col_cyan,  col_urgborder  },
-//};
+
+static const char norm_fg[] = "#b2b9b7";
+static const char norm_bg[] = "#080916";
+static const char norm_border[] = "#7c8180";
+
+static const char sel_fg[] = "#b2b9b7";
+static const char sel_bg[] = "#566162";
+static const char sel_border[] = "#b2b9b7";
+
+static const char *colors[][3]      = {
+    /*               fg           bg         border                         */
+    [SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins
+    [SchemeSel]  = { sel_fg,      sel_bg,    sel_border },  // the focused win
+};
 
 typedef struct {
 	const char *name;
