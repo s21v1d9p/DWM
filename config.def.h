9e5dac194009138257416538e106eb576cd6130c
s21v1d9p <svdp028@gmail.com>
2021-09-13 09:19:32 +0530

    antialias autohint added to jetbrains font

--- config.def.h
+++ config.def.h
@@ -20,7 +20,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = {	"JetBrains Mono SemiBold:size=11", "Noto Color Emoji:size=9:antialias=true:autohint=true" };
+static const char *fonts[]          = {	"JetBrains Mono SemiBold:size=11:antialias=true:autohint=true", "Noto Color Emoji:size=9:antialias=true:autohint=true" };
 
 static const char norm_fg[] = "#b2b9b7";
 static const char norm_bg[] = "#080916";
